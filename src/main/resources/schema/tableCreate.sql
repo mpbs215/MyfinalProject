97a6237ce322b23aabcc8a0d7e82b39562d3910f
Administrator <Administrator@MSDN-SPECIAL>
2018-06-12 12:44:37 +0900

    스키마 drop table 수정

--- src/main/resources/schema/tableCreate.sql
+++ src/main/resources/schema/tableCreate.sql
@@ -1,16 +1,16 @@
-drop table notice;
-drop table terms;
-drop table faq;
-drop table review;
-drop table parkImage;
-drop table carType;
-drop table authority;
-drop table seller;
-drop table qna;
-drop table parkReserve;
-drop table parkRegist;
-drop table Park;
-drop table UserInfo;
+drop table notice_TB;
+drop table terms_TB;
+drop table faq_TB;
+drop table review_TB;
+drop table parkImage_TB;
+drop table carType_TB;
+drop table authority_TB;
+drop table seller_TB;
+drop table qna_TB;
+drop table parkReserve_TB;
+drop table parkRegist_TB;
+drop table Park_TB;
+drop table UserInfo_TB;
 
 select reserveno,parkname,reservestart,reserveend,r.userid,price*(reserveend-reservestart)*24 price from park p join parkreserve r using(parkno) 
 where p.userid=0 and reserveend<sysdate
@@ -26,13 +26,6 @@ create table USER_INFO_TB
 		regidate		timestamp NOT NULL,
 		seller			char(1)
 );
-
-insert into userinfo values('KimD','1234','김돈황','mpbs215@naver.com','010-9324-9482','서울 은평구 불광동',sysdate,0);
-insert into userinfo values('KimM','1234','김민성','KimM@daum.com','010-9324-9482','대구 수성구',sysdate,0);
-insert into userinfo values('LeeJ','2345','이재문','LeeJaeDoor@naver.com','010-9324-9482','청주',sysdate,1);
-insert into userinfo values('JeeM','1234','지문경','moonkyong@naver.com','010-9324-9482','수원',sysdate,0);
-insert into userinfo values('KonE','3456','공은비','silverRain@naver.com','010-9324-9482','북한',sysdate,0);
-
 create table PARK_TB
 (
 		park_No				number 		   primary key,
@@ -45,20 +38,6 @@ create table PARK_TB
 		latitude			varchar2(100)  NOT NULL,
 		longitude			varchar2(100)  NOT NULL
 );
-
-insert into park values
-(park_seq.nextval,'재문주차장1호','LeeJ','성남시 분당구 정자동 178-1',
-'500.5','멋있는주차장',2000,5,'37.3594835','127.10520759999997')
-
-insert into park values
-(park_seq.nextval,'재문주차장2호','LeeJ','은평구 불광동 243-13번지 201',
-'50','부실한주차장',500,3,'37.6143768','126.93204160000005')
-
-insert into park values
-(park_seq.nextval,'재문주차장3호','LeeJ','서울특별시 종로구 송월길 48 서울특별시교육청',
-'5000','서울특별시교육청주차장',50000,10,'37.5701647','126.96724840000002')
-
-
 
 create table park_Regist_TB
 (
